63655812d17dcd4d53a0e9301e560cb541d8c371
vedant367 <vedant.cube56@gmail.com>
2026-04-12 23:01:00 +0530

    a

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // Bump VERSION every time you push an update.
 // This is what tells browsers "new version exists, throw away old cache".
-const VERSION = 'v5';
+const VERSION = 'v6';
 const CACHE = `smartgrow-${VERSION}`;
 
 const ASSETS = [
